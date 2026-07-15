1426775c8b260507adec7f6e48305cb9329b7bd5
takumasendeke <takumasendeke@gmail.com>
2026-07-15 14:30:06 +0200

    chore: purged build files

---
+++ build/0003_LongestSubstringWithoutRepeatingCharacters/CMakeFiles/LongestSubstringWithoutRepeatingCharacters.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LongestSubstringWithoutRepeatingCharacters.dir/solution.cpp.obj"
+  "CMakeFiles/LongestSubstringWithoutRepeatingCharacters.dir/solution.cpp.obj.d"
+  "LongestSubstringWithoutRepeatingCharacters.exe"
+  "LongestSubstringWithoutRepeatingCharacters.exe.manifest"
+  "LongestSubstringWithoutRepeatingCharacters.pdb"
+  "libLongestSubstringWithoutRepeatingCharacters.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LongestSubstringWithoutRepeatingCharacters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/0009_PalindromeNumber/CMakeFiles/palindromeNumber.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for palindromeNumber.
+# This may be replaced when dependencies are built.
