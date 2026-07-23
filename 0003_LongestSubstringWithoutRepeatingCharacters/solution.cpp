a9e88a83bb523ed5dce0293f3f897563addf091d
takumasendeke <takumasendeke@gmail.com>
2026-07-23 22:48:09 +0200

    feat: began working on the Median of 2 sorted arrays Problem

--- 0003_LongestSubstringWithoutRepeatingCharacters/solution.cpp
+++ 0003_LongestSubstringWithoutRepeatingCharacters/solution.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::string;
+using std::max;
 
 // helper func
-bool duplicate(const string& s,char c){
+bool duplicate(const string& substring,char c){
+    // edge case
+    if (substring.empty()){return false;}
+
     int counter = 0;
-    for (int i = 0; i < s.length(); i++ ){
-        if (s.at(i) == c){
-            return counter++;
+    for (int i = 0; i < substring.length(); i++ ){
+        if (substring.at(i) == c){
+            counter++;
         }
     }
 
@@ -22,21 +27,33 @@ int lengthOfLongestSubstring(const string& s) {
     if (s.empty()) {return 0;}
 
     int length = s.size();
-    int start = 0;
 
     string currentString = "";
+    int longest = 0;
 
     for (int end = 0; end < length; end++){
+        currentString += s.at(end);
         // TODO: implement logic
+        while (!duplicate(currentString,s.at(end))){
+            currentString += s.at(end);
+            longest = max(longest, (int)currentString.size());
+        }
+
+        if (duplicate(currentString,s.at(end))){
+            currentString.erase(s.begin());
+        }
+
     }
+
+    return longest;
 }
 
 int main(){
 
     string testCase = "bbbbb";
 
-    // cout << lengthOfLongestSubstring(testCase);
-    cout << duplicate("ass",'a');
+    cout << lengthOfLongestSubstring(testCase) << "\n";
+    cout << duplicate("assb",'b');
  
     return 0;
 }
---
+++ 0004_MedianOfTwoSortedArrays/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+
+double findMedianSortedArrays(const vector<int>& nums1,const vector<int>& nums2) {
+    int length1 = nums1.size();
+    int length2 = nums2.size()
+}
+
+int main(){
+
+    vector<int> nums1 = {1,3};
+    vector<int> nums2 = {2};
+
+    cout << findMedianSortedArrays(nums1,nums2);
+
+    return 0;
+}
