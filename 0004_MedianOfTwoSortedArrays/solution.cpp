6779a5cd20883153aad21df2958d58bca4ebafc9
takumasendeke <takumasendeke@gmail.com>
2026-07-25 23:41:28 +0200

    fix: small syntax correction, and though to implement AVL tree in order tpo solve problem

--- 0004_MedianOfTwoSortedArrays/solution.cpp
+++ 0004_MedianOfTwoSortedArrays/solution.cpp
@@ -8,7 +8,9 @@ using std::cout;
 // TODO: imlement logic
 double findMedianSortedArrays(const vector<int>& nums1,const vector<int>& nums2) {
     int length1 = nums1.size();
-    int length2 = nums2.size()
+    int length2 = nums2.size();
+
+    //use an AVL tree in C++
 }
 
 int main(){
